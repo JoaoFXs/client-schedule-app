09460d049d9c68c12cf8c305fb2e595ea4588435
jovibfel@gmail.com <jovibfel@gmail.com>
2025-11-24 17:20:17 -0300

    Tratamento para expiração de token

--- src/app/_shared/interceptors/auth.interceptor.ts
+++ src/app/_shared/interceptors/auth.interceptor.ts
@@ -1,5 +1,7 @@
-import { HttpInterceptorFn } from "@angular/common/http"; 
-
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 /**
  * Interceptador para adicionar Authorization bearer na requisição
  * @param req 
@@ -9,7 +11,12 @@ import { HttpInterceptorFn } from "@angular/common/http";
  */
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
+    const router = inject(Router);
+
     const token = localStorage.getItem('token');
+
+    // Clona a requisição se tiver token 
+    let authReq = req;
     //  Se o token existir, a requisição é clonada e adicionamos o cabeçalho de authenticação
     if (token){
         console.log(`Token utilizado no interceptador: Bearer ${token}`)
@@ -20,8 +27,29 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
                 }
             }
         );
-        return next(authReq)
     }
-  //  Se o token não existir, a requisição é feita sem o autorization
-    return next(req);
+    return next(authReq).pipe(
+        catchError((error: HttpErrorResponse) => {
+        
+        // Verifica se o erro é 401 (Unauthorized) ou 403 (Forbidden)
+        if (error.status === 401 || error.status === 403) {
+            
+            // Limpa o token inválido/expirado
+            localStorage.removeItem('meu-token');
+            
+            // Limpa outros dados do usuário
+            localStorage.removeItem('usuario-dados');
+
+            // Redireciona para o login
+            router.navigate(['/login']);
+            
+            // Mostra um alerta
+            // alert('Sua sessão expirou. Por favor, faça login novamente.');
+        }
+
+        // 3. Importante: Repassa o erro para quem chamou (o Componente)
+        // Isso permite que o componente pare o "loading", por exemplo.
+        return throwError(() => error);
+        })
+    );
 };
