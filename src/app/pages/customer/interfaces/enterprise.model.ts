372e8c61839622705d27373469784ba33408574d
João Felix <83048381+JoaoFXs@users.noreply.github.com>
2026-04-02 20:30:55 -0300

    feat: reestruturação codigo + getAllServices

---
+++ src/app/pages/customer/interfaces/enterprise.model.ts
@@ -0,0 +1,9 @@
+export interface Enterprise{
+    name: string;
+    description: string;
+    phone: string;
+    email: string;
+    website: string;
+    address: string;
+    cep: number;    
+}
--- src/app/pages/customer/main-search/main-search.component.ts
+++ src/app/pages/customer/main-search/main-search.component.ts
@@ -1,27 +1,40 @@
 import { Component } from '@angular/core';
 import { map, Observable, startWith } from 'rxjs';
 import { FormControl } from '@angular/forms';
-
+import { OnInit } from '@angular/core';
+import { MainSearchService } from '../services/main-search.service';
 @Component({
   selector: 'app-main-search',
   standalone: false,
   templateUrl: './main-search.component.html',
   styleUrl: './main-search.component.scss',
 })
-export class MainSearchComponent {
+export class MainSearchComponent implements OnInit {
   myControl = new FormControl('');
-  options: string[] = ['Cabelereiro', 'Manicure', 'Pedicure', 'Massagista', 'Esteticista'];
-  filteredOptions: Observable<string[]>;
 
-  constructor() {
+  filteredOptions: Observable<string[]>;
+  enterprises: any[] = []; // Aqui você pode definir a estrutura do seu array de empresas
+  
+  
+  
+  constructor(
+    private mainSearchService: MainSearchService
+  ) {
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
       map(value => this._filter(value || '')),
     );
   }
 
+  ngOnInit() {
+    this.mainSearchService.getAllServices().subscribe(data => {
+      this.enterprises = data; // Supondo que a resposta seja um array de empresas
+      console.log('Empresas recebidas:', this.enterprises);
+    });
+
+  }
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+    return this.enterprises.filter(enterprise => enterprise.toLowerCase().includes(filterValue));
   }
 }
